29aa92c7751790370e2f99fce36cf9fbf36f21c0
Anaximandr228 <alex09.g29@gmail.com>
2024-07-11 20:03:16 +0500

    second commit

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ bot = telebot.TeleBot(API_TOKEN)
 table_id = ''
 gc = gspread.service_account(filename='testtask-428911-6c0483131d7a.json')
 
-#Выставление счёта и его данных
+# Выставление счёта и его данных
 quickpay = Quickpay(
     receiver="4100118746332228",
     quickpay_form="shop",
@@ -18,6 +18,7 @@ quickpay = Quickpay(
     paymentType="SB",
     sum=2,
 )
+
 
 # Создание Inline-кнопок
 @bot.message_handler(commands=['start'])
@@ -37,20 +38,22 @@ def start(message):
 # Обработка callback-вызовов
 @bot.callback_query_handler(func=lambda call: True)
 def callback(call):
-    if call.data == 'pic':  #Обработка кнопки отправки изображения
+    if call.data == 'pic':  # Обработка кнопки отправки изображения
         bot.send_photo(call.message.chat.id, open('img1.png', 'rb'), caption="Текст")
 
     if call.data == 'getvalue':
-        sh = gc.open("гугл_табличка") #Обработка кнопки для получения значения из Google таблиц
+        sh = gc.open("гугл_табличка")  # Обработка кнопки для получения значения из Google таблиц
         cell = (sh.sheet1.get('A2'))
         bot.send_message(call.message.chat.id, text=cell)
 
     if call.data == 'checkdate':
-        bot.send_message(call.message.chat.id, 'Введите дату для проверки') #Обработка кнопки для проверки и записи даты  Google таблицы
+        bot.send_message(call.message.chat.id,
+                         text='Введите дату для проверки')  # Обработка кнопки для проверки и записи даты  Google таблицы
         bot.register_next_step_handler(call.message, check_date)
+        bot.send_message(call.message.chat.id, text='Дата верна')
 
 
-# Функция проверки правильности даты и записи в Google таблицы
+# Функция проверки правильности даты
 def check_date(message):
     try:
         input_date = message.text
@@ -58,10 +61,13 @@ def check_date(message):
         datetime.datetime.strptime(input_date, date_format)
     except ValueError:
         bot.send_message(message.chat.id, 'дата неверна')
+    return input_date
 
+
+# Запись данных в Google таблицу
+def fill_date(date):
     sh = gc.open("гугл_табличка")
-    sh.sheet1.update_cell(2, 2, input_date)
-    bot.send_message(message.chat.id, text="Дата верна")
+    sh.sheet1.update_cell(2, 2, date)
 
 
 bot.polling(none_stop=True)
